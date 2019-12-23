306d059b087c63a9320035d23d3914b8856206fb
Augustino Nguyen <trongphu.cbl49@gmail.com>
2019-12-23 15:54:24 +0700

    Cart update view

--- ShoeStore/products/views.py
+++ ShoeStore/products/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
+from carts.models import Cart
 
 from .models import Product
 
@@ -45,6 +46,13 @@ def product_list_view(request):
 class ProductDetailSlugView(DetailView):
     queryset = Product.objects.all()
     template_name = "products/product-detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
 
     def get_object(self, *args, **kwargs):
         request = self.request
