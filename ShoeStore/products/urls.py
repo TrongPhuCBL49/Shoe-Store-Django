8f60eb1f5f50b0cdd272c7288fb1b0e62d3ab50c
Augustino Nguyen <trongphu.cbl49@gmail.com>
2019-12-23 13:37:48 +0700

    URLs Product done

--- ShoeStore/products/urls.py
+++ ShoeStore/products/urls.py
@@ -12,12 +12,12 @@ from .views import (
 app_name = 'products'
 
 urlpatterns = [
-    path('products/', ProductListView.as_view(), name='products'),
+    path('', ProductListView.as_view(), name='products'),
     path('products-fbv/', product_list_view, name='products-fbv'),
     path('products/<int:pk>', ProductDetailView.as_view(), name='products-detail'),
     path('products-fbv/<int:pk>', product_detail_view, name='products-fbv-detail'),
     path('feature-products/', ProductFeaturedListView.as_view(), name='feature-products'),
     path('feature-products/<int:pk>', ProductFeaturedDetailView.as_view(), name='feature-products-detail'),
-    path('products/<slug:slug>', ProductDetailSlugView.as_view(), name='products-slug-detail'),
+    path('<slug:slug>/', ProductDetailSlugView.as_view(), name='detail'),
     
 ]
