9c379671d2a3b7ed90137f4e99faa2b3a6d71b66
Augustino Nguyen <trongphu.cbl49@gmail.com>
2020-01-06 22:35:53 +0700

    Generate the Order ID

--- ShoeStore/products/models.py
+++ ShoeStore/products/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import pre_save, post_save
 from django.urls import reverse
 from django.db.models import Q
 
-from .utils import unique_slug_generator
+from ShoeStore.utils import unique_slug_generator
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
